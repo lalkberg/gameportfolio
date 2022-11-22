4af37b914af946466bf88bf5434f4e926052b334
Leo Alkberg <leo.alkberg@gmail.com>
2022-11-23 00:40:16 +0100

    Updated pages to use GetMarkdown

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import {
-    Link
-  } from "react-router-dom";
+import MarkDown from 'markdown-to-jsx'
+import GetMarkdown from './GetMarkdown'
 
-  const Home = () => {
-    return (
-      <div>
-        GAMEPLAY PROGRAMMER
-        TECHNICAL DESIGNER
+const Home = () => {
+  const md = 'home.md'
+
+  return (
+    <div className="box">
+      <div className= "container">
+        <MarkDown className="markdown">
+          {GetMarkdown(md)}
+        </MarkDown>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Home;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import MarkDown from 'markdown-to-jsx'
+import GetMarkdown from './GetMarkdown'
 
 export default function Portfolio() {
+  const md = 'portfolio.md'
+
   return (
-    <div>Portfolio</div>
-  )
+    <div className="box">
+      <div className= "container">
+        <MarkDown className="markdown">
+          {GetMarkdown(md)}
+        </MarkDown>
+      </div>
+    </div>
+  );
 }
