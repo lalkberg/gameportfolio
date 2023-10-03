80a2f1722d60ee70c75e43e5c8f644bbade585db
lalkberg <leo.alkberg@gmail.com>
2023-10-03 17:11:29 +0200

    Buncha stuff

--- src/Nav.js
+++ src/Nav.js
@@ -5,16 +5,18 @@ import backgroundVideo from './header_stefwithanf.mp4'
 const Nav = () => {
     return(
         <div className='nav'>
-            LEO ALKBERG
+            <div className='header-video'>
+                <video autoPlay muted loop id="myVideo">
+                    <source src={backgroundVideo} type="video/mp4"></source>
+                </video>
+            </div>
+            <div className='header-name'>
+                <text myName>LEO ALKBERG</text>
+            </div>
             <div className='nav-buttons'>
                 <Link to="/"><button className='button-28'>HOME</button></Link>
                 <Link to="/portfolio"><button className='button-28'>PORTFOLIO</button></Link>
                 <Link to="/contact"><button className='button-28'>CONTACT</button></Link>
-                <div className='header-video'>
-                    <video autoPlay muted loop id="myVideo">
-                        <source src={backgroundVideo} type="video/mp4"></source>
-                    </video>
-                </div>
             </div>
         </div>
     )
